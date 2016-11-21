e60cc0d0a6e38b4e42511a29f4cf08cd03e61fff
Jonathan Garay <jonathan@devmask.net>
2016-11-21 13:10:02 -0600

    added force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :cuandoesquincena, Cuandoesquincena.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "cuandoesquincena.com", port: 80],
+  url: [scheme: "https", host: "cuandoesquincena.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
