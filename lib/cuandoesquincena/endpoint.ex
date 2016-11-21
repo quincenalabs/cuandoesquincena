3377134b060090e999827218a1a11bba3e433008
Jonathan Garay <jonathan@devmask.net>
2016-11-21 13:38:45 -0600

    added le cert

--- lib/cuandoesquincena/endpoint.ex
+++ lib/cuandoesquincena/endpoint.ex
@@ -9,7 +9,7 @@ defmodule Cuandoesquincena.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :cuandoesquincena, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt .well-known)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
