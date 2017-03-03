51bf8e767369cc69a4ce9bd3c988261b22c8e1b4
Jonathan Garay <jonathan@devmask.net>
2017-03-03 01:33:38 -0600

    remove from brunch

--- brunch-config.js
+++ brunch-config.js
@@ -9,8 +9,7 @@ exports.config = {
           "js/jquery.min.js": ["web/static/vendor/jquery.min.js"],
           "js/soon.min.js": ["web/static/vendor/soon.min.js"],
           "OneSignalSDKUpdaterWorker.js" : ["web/static/vendor/OneSignalSDKUpdaterWorker.js"],
-          "OneSignalSDKWorker.js" : ["web/static/vendor/OneSignalSDKWorker.js"],
-          "manifest.json" : ["web/static/manifest.json"]
+          "OneSignalSDKWorker.js" : ["web/static/vendor/OneSignalSDKWorker.js"]
       }
     },
     stylesheets: {
